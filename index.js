8eb465ba067a3fa07742bbe5546128af8d601175
Sofia Mercedes <cansinosofia72@gmail.com>
2025-02-20 07:25:27 -0600

    Signed-off-by: Sofia Mercedes <cansinosofia72@gmail.com>

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function ocultarElemento (idElemeto) {
+    let elemento=document.getElementById (idElemeto)
+    console.log (elemento)
+    elemento.style.display="none"
+}
+
+function mostrarElemento (idElemeto) {
+    let elemento=document.getElementById (idElemeto)
+    console.log (elemento)
+    elemento.style.display="block"
+}
